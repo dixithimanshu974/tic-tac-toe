8ffcf10d7855ba617290735b801fb54d363f210b
Himanshu Dixit <himanshu@realbetter.com>
2025-09-17 13:35:18 +0530

    changed next config file

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/tic-tac-toe',
-  assetPrefix: '/tic-tac-toe/',
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
