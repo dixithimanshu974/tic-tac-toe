d218a3a2f248edfcd3c68ca6d6cdfe0a3e9640e8
Himanshu Dixit <himanshu@realbetter.com>
2025-09-17 12:55:59 +0530

    Updates

---
+++ _next/static/chunks/acaa6d9a344d3c95.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/047ac4b9b0bd36da.js",
+  "static/chunks/469d7efdf6c19030.js",
+  "static/chunks/turbopack-c3fdc4170b170843.js"
+])
---
+++ _next/static/ulvDzHmw-ZSn2geZFaiR8/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/0959be56d8f8d7b1.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/tic-tac-toe//_next/:path+",
+        "destination": "/tic-tac-toe/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
